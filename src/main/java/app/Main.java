da19fd3484923e6d58d63540c57db09b49ff57f5
poketopa <lhs5427ll@gmail.com>
2025-11-12 01:54:32 +0900

    chore(main): 진입점 유지 보수

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,15 +1,16 @@
-package main.java.app.cli;
+package app;
 
-import main.java.app.config.Appconfig;
-import main.java.app.controller.GitController;
+import app.config.Appconfig;
+import app.controller.GitController;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class Main {
     public static void main(String[] args) {
-        Path cwd = Paths.get(System.getProperty("user.dir"));
-        GitController controller = new Appconfig(cwd).gitController();
+        // 현재 작업 디렉토리 경로를 반환
+        Path path = Paths.get(System.getProperty("user.dir"));
+        GitController controller = new Appconfig(path).gitController();
         controller.run(args);
     }
 }
