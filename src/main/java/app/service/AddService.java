7fe2a33932fa8530d65e19bc5b8a8aa785820adc
poketopa <lhs5427ll@gmail.com>
2025-11-11 15:15:05 +0900

    feat(service): AddService, InitService 인터페이스 생성

---
+++ src/main/java/app/service/AddService.java
@@ -0,0 +1,8 @@
+package app.service;
+
+import java.util.List;
+
+public interface AddService {
+    void add(List<String> paths);
+}
+
---
+++ src/main/java/app/service/InitService.java
@@ -0,0 +1,6 @@
+package app.service;
+
+public interface InitService {
+    void init();
+}
+
